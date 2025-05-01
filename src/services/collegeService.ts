a491f5f965c628b6d12162d8b673703dc44a9fb6
faraz-wq <faraz40684@gmail.com>
2025-05-01 17:24:06 +0530

    api call through env

--- src/services/collegeService.ts
+++ src/services/collegeService.ts
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/colleges';
+const API_URL = `${import.meta.env.VITE_API_URL}/colleges`;
 
 export interface College {
   _id: string;
